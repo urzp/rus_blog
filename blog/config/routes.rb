79c9e0ae345ab904fd8c10b6a72f6297b4050457
Paul <urzp002@mail.ru>
2016-04-18 13:15:29 +0400

    Add resources articles

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+  # root 'welcome#index'
+   
+   Rails.application.routes.draw do
+ 
+		resources :articles
+ 
+		root 'welcome#index'
+	end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
